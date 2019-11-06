cdecabd2dc46bbd8129982e541968474e60c4244
rahmeh <rahmeh.tartouri@gmail.com>
2019-11-07 00:31:28 +0200

    third script is done

--- js/operation.js
+++ js/operation.js
@@ -72,9 +72,103 @@ var contentDivs = new Array();
     element.appendChild(para);
     
     i=i+1;
-  
-
-
   } 
+
+
+  /* Start Content */
+
+  (function () {
+    'use strict';
+    
+    var draggedEl,
+        onDragStart,
+        onDrag,
+        onDragEnd,
+        grabPointY,
+        grabPointX,
+        createNote,
+        addNoteBtnEl;
+    
+    onDragStart = function (ev) {
+      var boundingClientRect;
+      if (ev.target.className.indexOf('textareas') === -1) {
+        return;
+      }
+      
+      draggedEl = this;
+      
+      boundingClientRect = draggedEl.getBoundingClientRect();
+      
+      grabPointY = boundingClientRect.top - ev.clientY;
+      grabPointX = boundingClientRect.left - ev.clientX;
+    };
+    
+    onDrag = function (ev) {
+      if (!draggedEl) {
+        return;
+      }
+      
+      var posX = ev.clientX + grabPointX,
+          posY = ev.clientY + grabPointY;
+      
+      if (posX < 0) {
+        posX = 0;
+      }
+      
+      if (posY < 0) {
+        posY = 0;
+      }
+      
+      draggedEl.style.transform = "translateX(" + posX + "px) translateY(" + posY + "px)";
+    };
+    
+    onDragEnd = function () { 
+      draggedEl = null;
+      grabPointX = null;
+      grabPointY = null;
+    };
+    
+    createNote = function () {
+      var stickerEl = document.createElement('div'),
+          footerEl = document.createElement('div'),
+          textareaEl = document.createElement('textarea');
+      
+      var transformCSSValue = "translateX(" + Math.random() * 400 + "px) translateY(" + Math.random() * 400 + "px)";
+      
+      stickerEl.style.transform = transformCSSValue; 
+      
+      footerEl.classList.add('footer');
+      textareaEl.classList.add('textareas');
+      stickerEl.classList.add('sticker');
+      
+      stickerEl.appendChild(footerEl);
+      stickerEl.appendChild(textareaEl); 
+      
+      stickerEl.addEventListener('mousedown', onDragStart, false);
+      
+      document.body.appendChild(stickerEl);
+    };
+    
+    createNote(); 
+    
+    addNoteBtnEl = document.querySelector('.addNoteBtn');
+    addNoteBtnEl.addEventListener('click', createNote, false);
+    document.addEventListener('mousemove', onDrag, false);
+    document.addEventListener('mouseup', onDragEnd, false);
+  })();
+
+
+
+
+
+
+
+
+
+
+
+
+
+  /* End Content */
  
   
